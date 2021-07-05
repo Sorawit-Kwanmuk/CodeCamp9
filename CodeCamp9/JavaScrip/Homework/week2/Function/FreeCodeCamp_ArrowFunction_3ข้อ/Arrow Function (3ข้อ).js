2fd34715ead11da1343969f57df018cb3efa0473
SorawitKwanmuk <sorawit_kwanmuk@outlook.com>
2021-07-05 20:14:33 +0700

    java week3 homework

--- CodeCamp9/JavaScrip/Homework/week2/Function/FreeCodeCamp_ArrowFunction_3ข้อ/Arrow Function (3ข้อ).js
+++ CodeCamp9/JavaScrip/Homework/week2/Function/FreeCodeCamp_ArrowFunction_3ข้อ/Arrow Function (3ข้อ).js
@@ -7,11 +7,11 @@
 
 // 2.Write Arrow Functions with Parameters
 // const myConcat = (arr1, arr2) => {
-//     "use strict";
-//     return arr1.concat(arr2);
-//   };
-//   // test your code
-//   console.log(myConcat([1, 2], [3, 4, 5]));
+//   "use strict";
+//   return arr1.concat(arr2);
+// };
+// // test your code
+// console.log(myConcat([1, 2], [3, 4, 5]));
 
 // 3.Set Default Parameters for Your Functions
 // const increment = (number, value = 1) => number + value;
--- CodeCamp9/JavaScrip/Homework/week2/Function/FreeCodeCamp_Function_Part1_9ข้อ/Function(9ข้อ).js
+++ CodeCamp9/JavaScrip/Homework/week2/Function/FreeCodeCamp_Function_Part1_9ข้อ/Function(9ข้อ).js
@@ -90,5 +90,17 @@
 
 // Application
 // 1.Stand in Line (ใช้ความรู้เรื่อง Array)
-// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/#basic-javascript
-// ?
+// function nextInLine(arr, item) {
+//   // Only change code below this line
+//   arr.push(item);
+//   var item = arr.shift();
+//   return item;
+//   // Only change code above this line
+// }
+// // Setup
+// var testArr = [1, 2, 3, 4, 5];
+
+// // Display code
+// console.log("Before: " + JSON.stringify(testArr));
+// console.log(nextInLine(testArr, 6));
+// console.log("After: " + JSON.stringify(testArr));
