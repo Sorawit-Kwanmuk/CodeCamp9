8bb149bc6429de90ac11f7f99397db04316aab56
Sorawit_Kwanmuk <sorawit_kwanmuk@outlook.com>
2021-07-08 17:27:54 +0700

    LAB 4.9

---
+++ CodeCamp9/JavaScrip/Browser Document and Event/Browser Event/main.js
@@ -0,0 +1,88 @@
+// LAB4.2
+// let formAdd = document.querySelector("#myForm");
+
+// formAdd.addEventListener("submit", alertMessage);
+// formAdd.addEventListener("submit", (e) => e.preventDefault());
+// setTimeout(() => formAdd.removeEventListener("submit", alertMessage), 5000);
+
+// function alertMessage() {
+//   alert("Form is submitted");
+// }
+
+// LAB4.3
+// let formAdd2 = document.querySelector("#myForm2");
+// formAdd2.addEventListener("submit", (e) => e.preventDefault());
+
+// LAB4.4
+// let link = document.querySelector("#link");
+// link.addEventListener("click", stop);
+
+// function stop(e) {
+//   let answer = confirm("Leave for https://google.com?");
+//   if (answer === false) {
+//     e.preventDefault();
+//   }
+// }
+
+// LAB4.5
+// let input1 = document.querySelector("#input1");
+// let input2 = document.querySelector("#input2");
+
+// input1.addEventListener("focus", onFocus);
+// input1.addEventListener("input", onInput);
+// input2.addEventListener("focus", onFocus2);
+// input2.addEventListener("input", onInput2);
+
+// function onFocus() {
+//   return console.log(input1.name);
+// }
+// function onInput() {
+//   return console.log(input1.value);
+// }
+// function onFocus2() {
+//   return console.log(input2.name);
+// }
+// function onInput2() {
+//   return console.log(input2.value);
+// }
+
+// LAB4.6
+// let button = document.querySelector("#button");
+// button.addEventListener("click", hide);
+// function hide() {
+//   if (button.style.display === "none") {
+//     button.style.display = "block";
+//   } else {
+//     button.style.display = "none";
+//   }
+// }
+
+// LAB4.7
+// let button = document.querySelector("#button");
+// let div = document.querySelector("#div");
+// button.addEventListener("click", hide);
+// function hide() {
+//   if (div.style.display === "none") {
+//     div.style.display = "block";
+//   } else {
+//     div.style.display = "none";
+//   }
+// }
+
+// LAB4.8
+// let button = document.querySelector("#button");
+// button.addEventListener("click", switchText);
+
+// function switchText() {
+//   if (button.innerHTML === "Hide") {
+//     button.innerHTML = "Show";
+//   } else {
+//     button.innerHTML = "Hide";
+//   }
+// }
+
+// LAB4.9
+let input = document.querySelector("#input");
+let button = document.querySelector("#button");
+
+input.addEventListener();
---
+++ CodeCamp9/JavaScrip/Browser Document and Event/Document Object Model/Document Object Model.js
@@ -0,0 +1,10 @@
+// LAB1
+// let div1 = document.createElement("div");
+// div1.textContent = "User:";
+// console.log(div1);
+// let ul1 = document.createElement("ul");
+// ul1.appendChild(document.createElement('li')).textContent = 'John';
+// ul1.appendChild(document.createElement('li')).textContent = 'Pete';
+// console.log(ul1);
+// let element = document.get
+// LAB2
