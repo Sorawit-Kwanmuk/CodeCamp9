c80d113436009315df188b00ec7ef11d53ea74ab
SorawitKwanmuk <sorawit_kwanmuk@outlook.com>
2021-07-16 12:11:19 +0700

    7/16/2021 day5 study from home

--- CodeCamp9/test/main.js
+++ CodeCamp9/test/main.js
@@ -93,19 +93,19 @@
 // console.log(reverseStr(1023));
 
 //function Greatest common divisor
-function gcd(a, b) {
-  if (b === 0) {
-    return a;
-  }
-  return gcd(b, a % b);
-}
-console.log(gcd(12, 18));
+// function gcd(a, b) {
+//   if (b === 0) {
+//     return a;
+//   }
+//   return gcd(b, a % b);
+// }
+// console.log(gcd(12, 18));
 
-// function Least common multiple
-function lcm(a, b) {
-  return (a * b) / gcd(a, b);
-}
-console.log(lcm(12, 18));
+// // function Least common multiple
+// function lcm(a, b) {
+//   return (a * b) / gcd(a, b);
+// }
+// console.log(lcm(12, 18));
 
 // function gcd(a, b) {
 //   const max = a < b ? a : b;
@@ -199,3 +199,103 @@ console.log(lcm(12, 18));
 // }
 
 // console.log(changeUpper(str));
+
+// !!!!!!!!!!!!!!! OBJECT !!!!!!!!!!!!!!!
+// Clone Object
+// const ping = {
+//   phone: "0622341385",
+//   address: "Jalan Kaki",
+// };
+// const mint = { phone: "0805555555", address: "Jalan Kaki", food: "soup" };
+// const jame = {};
+// Object.assign(jame, ping, mint);
+// jame.phone = "0949443468";
+// console.log(jame);
+// console.log(ping);
+// console.log(mint);
+// const jame2 = { ...ping, ...mint };
+//เอาvalueตัวหลังไปใส่ไว้ใน jame2 จากนั้นเอาvalue ของ ping ไปอัพเดทค่าของjame2 ถ้ามีค่าkeyตรงกัน ถ้าkeyไม่ตรงกันจะเพิ่มkeyตัวใหม่
+
+// Object bm
+//คือสามารถมี function เป็น valueได้
+
+// let user = {
+//   name: "John",
+//   age: 30,
+//   sayHi() {
+//     alert(this.name + " " + this.age);
+//     //สามารถใช้ user.name ได้ แต่ถ้าเรา clone object จะใช้งานไม่ได้ Hardcode ไม่ยืดหยุ่นตามสิ่งที่มันควรจะเป็น
+//   },
+// };
+// user.sayHi();
+// let admin = { ...user };
+// admin.name = "P'Ying";
+// console.log(admin);
+// admin.sayHi();
+
+// !!!!!!!!!!!!!!!!!!!!this + Constructor function!!!!!!!!!!!!!!!!!!!!!!
+//EX normal fucntion
+// function User(a, b, c) {
+//   firstName = a;
+//   lastName = b;
+//   age = c;
+// }
+//ถ้าเราต้องการสร้าง Object จากฟังก์ชั่น constructor User, Object ที่ถูกสร้างขึ้น จะมี property คาม
+//ที่กำหนดไว้ในฟังก์ชั่น constructor User นั่นเอง
+//EX ฟังก์ชั่น constructor function
+// function User(a, b, c) {
+//   //this = {}
+//   this.firstName = a;
+//   this.lastName = b;
+//   this.age = c;
+//   this.dayOlds = function () {
+//     return this.age * 365.25;
+//   };
+//   //return this;
+// }
+// const ice = new User("Ice", "Cream", 22);
+// //ice = function(){return{firstName: "Ice", lastName: "Cream", age: 22}};
+// console.log(ice);
+
+// for (let key in ice) {
+//   console.log("key: ", key);
+//   console.log("value: ", ice[key]);
+// }
+// console.log(ice.dayOlds());
+
+// const boon = new User("Boon", "Chai", 25);
+// consile.log(boon.dayOlds());
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!Optionnal chaining!!!!!!!!!!!!!!!!!!!!!!!!!
+//สำคัญมาก ใช้ใน react
+//EX
+// let user = {};
+// console.log(user.address.province); //error เพราะ user เป็น objectเปล่า ทำให้.address เป็น undefined .province ดึงข้อมูลจาก undefined จึง error
+// console.log(user.address?.province); //ป้องกันการerrorของข้อมูล user.มีadressไหมถ้ามีไปหา.provinceต่อ ถ้าไม่ หยุดการค้นหา
+
+// const toon = {
+//   name = "Nuttapat",
+//   weight = 70,
+//   height = 180,
+//   bmi: function () {
+//     return this.weight / (this.height/100)**2;
+//   },
+// }
+// console.log(toon.bmi());
+// toon.weight = 75;
+// console.log(toon.bmi());
+
+//constructor function
+// function calculateBMI(name, w, h) {
+//   this.name = name;
+//   this.weight = w;
+//   this.height = h;
+//   this.bmi = function () {
+//     return this.weight / (this.height / 100) ** 2;
+//   };
+//   return this;
+// }
+// const toon = new calculateBMI("Nuttapat", 70, 180);
+// console.log(toon.bmi());
+// const ohm = new calculateBMI("ohmnaja", 58, 169);
+// console.log(ohm.bmi());
