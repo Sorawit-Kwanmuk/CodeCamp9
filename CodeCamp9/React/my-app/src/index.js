070f8cae19ca9be94547f67d2d6a306bf38bfe11
SorawitKwanmuk <sorawit_kwanmuk@outlook.com>
2021-07-30 14:57:01 +0700

    create first React App project edit

--- CodeCamp9/React/my-app/src/index.js
+++ CodeCamp9/React/my-app/src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
+// import { Welcome, Greeting, PI } from "./component/Welcome";
+// import * as W from "./component/Welcome";
+// import MyComponent from "./component/Welcome";
+import MyComponent, { Welcome, Greeting, PI } from "./component/Welcome";
+// function App1() {
+//   return <div>Hello React</div>;
+// }
+function App2() {
+  return (
+    <div>
+      {/* <W.Welcome />
+      <W.Greeting /> */}
+      <Welcome />
+      <Greeting />
+      <MyComponent />
+    </div>
+  );
+}
+console.log(PI);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App2 />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
