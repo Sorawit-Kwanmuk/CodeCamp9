9a7ce0f92aa02b6b0ba508a73ec7a9050235b146
SorawitKwanmuk <sorawit_kwanmuk@outlook.com>
2021-08-02 09:27:13 +0700

    complete Lab4.3

--- CodeCamp9/React/my-app/src/App.js
+++ CodeCamp9/React/my-app/src/App.js
@@ -1,38 +1,38 @@
-import "./App.css";
+import './App.css';
 function App() {
   return (
-    <div style={{ padding: "2rem" }}>
+    <div style={{ padding: '2rem' }}>
       <button
         style={{
-          padding: "0.375rem 0.75rem",
-          border: "1px solid black",
-          borderRadius: "4px",
-          backgroundColor: "#555555",
-          color: "#fff",
-          marginRight: "1rem",
+          padding: '0.375rem 0.75rem',
+          border: '1px solid black',
+          borderRadius: '4px',
+          backgroundColor: '#555555',
+          color: '#fff',
+          marginRight: '1rem',
         }}>
         +
       </button>
-      <span style={{ marginRight: "1rem" }}>0</span>
+      <span style={{ marginRight: '1rem' }}>0</span>
       <button
         style={{
-          padding: "0.375rem 0.75rem",
-          border: "1px solid black",
-          borderRadius: "4px",
-          backgroundColor: "#555555",
-          color: "#fff",
-          marginRight: "1rem",
+          padding: '0.375rem 0.75rem',
+          border: '1px solid black',
+          borderRadius: '4px',
+          backgroundColor: '#555555',
+          color: '#fff',
+          marginRight: '1rem',
         }}>
         -
       </button>
       <button
         style={{
-          padding: "0.375rem 0.75rem",
-          border: "1px solid black",
-          borderRadius: "4px",
-          backgroundColor: "#555555",
-          color: "#fff",
-          marginRight: "1rem",
+          padding: '0.375rem 0.75rem',
+          border: '1px solid black',
+          borderRadius: '4px',
+          backgroundColor: '#555555',
+          color: '#fff',
+          marginRight: '1rem',
         }}>
         Reset
       </button>
