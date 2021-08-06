d9e86083166e93fd9859f504bf1afb26ca79d7f0
SorawitKwanmuk <sorawit_kwanmuk@outlook.com>
2021-08-06 09:10:40 +0700

    lab8.4 fix

--- CodeCamp9/React/my-app/src/App.js
+++ CodeCamp9/React/my-app/src/App.js
@@ -1,126 +1,63 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  //Lab8.1
-  // const friendList = ['John', 'Jame', 'Anna', 'Brion'];
-  // const list = friendList.map((element,index) => <li>{element}</li>);
-  //Lab8.2
-  // const arrayPost = [
-  //   {
-  //     id: '1',
-  //     message: 'Lorem, ipsum dolor1.',
-  //     Date: '1/1/2017',
-  //     author: 'John',
-  //   },
-  //   {
-  //     id: '2',
-  //     message: 'Lorem, ipsum dolor2.',
-  //     Date: '2/8/2017',
-  //     author: 'Jame',
-  //   },
-  //   {
-  //     id: '3',
-  //     message: 'Lorem, ipsum dolor3.',
-  //     Date: '12/4/2017',
-  //     author: 'Anna',
-  //   },
-  //   {
-  //     id: '4',
-  //     message: 'Lorem, ipsum dolor4.',
-  //     Date: '12/12/2017',
-  //     author: 'Brion',
-  //   },
-  // ];
-  // const list = arrayPost.map(item => (
-  //   <li key={item.id}>
-  //     Name: {item.author}
-  //     <br />
-  //     Date: {item.Date}
-  //     <br />
-  //     Message: {item.message}
-  //     <br />
-  //     <br />
-  //   </li>
-  // ));
-  //Lab8.3 version1
-  // const arrayData = [
-  //   {
-  //     id: '1',
-  //     message: 'Happy new year',
-  //     author: 'John',
-  //     date: '1/1/2021 00.01AM',
-  //   },
-  //   {
-  //     id: '2',
-  //     message: 'Programing is fun',
-  //     author: 'Jack',
-  //     date: '2/8/2017 10.23AM',
-  //   },
-  //   {
-  //     id: '3',
-  //     message: 'Oh Amazing !!!',
-  //     author: 'Sarah',
-  //     date: '12/4/2017 08.17PM',
-  //   },
-  // ];
-  // const list = arrayData.map(item => (
-  //   <div key={item.id} className='divStyle'>
-  //     <div>
-  //       <div className='divTop'>
-  //         <h3>{item.message}</h3>
-  //       </div>
-  //     </div>
-  //     <div className='divButtom'>
-  //       <div>{item.author}</div>
-  //       <div>{item.date}</div>
-  //     </div>
-  //   </div>
-  // ));
-  // Lab8.3 version2 better
-  // const post = [
-  //   {
-  //     id: '1',
-  //     message: 'Happy new year',
-  //     author: 'John',
-  //     date: '1/1/2021 00.01AM',
-  //   },
-  //   {
-  //     id: '2',
-  //     message: 'Programing is fun',
-  //     author: 'Jack',
-  //     date: '2/8/2017 10.23AM',
-  //   },
-  //   {
-  //     id: '3',
-  //     message: 'Oh Amazing !!!',
-  //     author: 'Sarah',
-  //     date: '12/4/2017 08.17PM',
-  //   },
-  // ];
-  // function Post({ post: { message, author, date } }) {
-  //   return (
-  //     <div
-  //       style={{
-  //         border: '1px solid black',
-  //         padding: '1rem',
-  //         marginBottom: '1rem',
-  //       }}>
-  //       <h2>{message}</h2>
-  //       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-  //         <span>Author: {author}</span>
-  //         <span>{date}</span>
-  //       </div>
-  //     </div>
-  //   );
-  // }
+  const [list, setList] = useState([
+    { id: 1, task: 'Job1', status: 'todo' },
+    { id: 2, task: 'Job2', status: 'todo' },
+    { id: 3, task: 'Job3', status: 'todo' },
+    { id: 4, task: 'Job4', status: 'todo' },
+    { id: 5, task: 'Job5', status: 'doing' },
+    { id: 6, task: 'Task1', status: 'todo' },
+    { id: 7, task: 'Task2', status: 'todo' },
+    { id: 8, task: 'Task3', status: 'doing' },
+    { id: 9, task: 'Task4', status: 'doing' },
+    { id: 10, task: 'Task5', status: 'done' },
+  ]);
+  const [in1, setIn1] = useState('');
+
+  const hdlIn1Change = e => {
+    setIn1(e.target.value);
+  };
+
+  const hdlAdd = () => {
+    let newId = list[list.length - 1].id + 1;
+    const newList = [...list];
+    let newTask = { id: newId, task: in1, status: 'todo' };
+    newList.push(newTask);
+    setList(newList);
+  };
+
+  const deleteLast = e => {
+    const newList = [...list];
+    newList.pop();
+    setList(newList);
+  };
+
+  const hdlDelete = del_id => {
+    console.log(del_id);
+    const newList = [...list];
+    let idx = newList.findIndex(x => x.id === del_id);
+    console.log(idx);
+    newList.splice(idx, 1);
+    setList(newList);
+  };
+
   return (
-    // lab8.3
-    // <>
-    //   {post.map(item => (
-    //     <Post key={item.id} post={item} />
-    //   ))}
-    // </>
-    <div></div>
+    <div className='App'>
+      <h3>List all Jobs</h3>
+      <input onChange={hdlIn1Change} value={in1} />
+      <button onClick={hdlAdd}>Add</button>
+      <ol>
+        {list.map(x => (
+          <li key={x.id}>
+            ID : {x.id} , Task : {x.task}
+            <button onClick={() => hdlDelete(x.id)}>X</button>
+          </li>
+        ))}
+      </ol>
+      <button onClick={deleteLast}>delete last one</button>
+    </div>
   );
 }
 
