d1e3f909aed078682ab12dafec5431e29b3b55a0
SorawitKwanmuk <sorawit_kwanmuk@outlook.com>
2021-08-26 10:43:25 +0700

    componentEven ต่างๆ

--- CodeCamp9/React/hook/src/App.js
+++ CodeCamp9/React/hook/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Component } from 'react';
+import { Component, useEffect } from 'react';
 import CountText from './components/CountText';
 
 // function App() {
@@ -34,25 +34,44 @@ import CountText from './components/CountText';
 // objA.sayHi();
 
 class App extends Component {
+  // class ประกอบด้วย property และ method กี่ตัวก็ได้
   // App ไปสืบทอดทุกสิ่ง มาจาก class Component
   //#1 Declare state
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     count: 0,
-  //     error: '',
-  //   };
-  // }
+  constructor(props) {
+    super(props);
+    //   this.state = {
+    //     count: 0,
+    //     error: '',
+    //   };
+    // this.handleClickReset = this.handleClickReset.bind(this); // fix lost this
+    console.log('App: Constrctor');
+  }
   //#2 ประกาศเป็น property ตัวนึง
+
   state = {
     count: 0,
     error: '',
+    showCounter: true,
   };
 
+  // handleClichReset () {
+  //   this.setState({ count: 0 });
+  // }; ถ้าไม่เขียนเป็น arrow function จะทำให้มันทำงานไม่ได้ เพราะมันไม่รู้จัก this หือก็คือเกิด lost this แก้โดยการไป bind(this) ใน constructor
+
+  //#3 ประกาศเป็น method ตัวนึง
   handleClichIncrement = () => {
     //increse count by 1
     this.setState({ count: this.state.count + 1 }); // new state : {count:1,error:''}
+    //จริงๆแล้วการเขียนแบบนี้มันไม่ดี เพราะมันอิงค่าจาก state ปัจจุบัน ถ้าใน function
+    //มีการเรียก this.setState({ count: this.state.count + 1 }); อีกอันนึง count จะเพิ่มแค่1 ไม่ได้เพิ่ม2
+
+    //setState ได้มาจาก class App ที่มีการสืบทอดทุกสิ่ง มาจาก class Component
+    // this.setState(current => ({ count: current.count + 1 }));
+    //แบบนี่คือวิธีที่ดี เพราะมันอิงค่าจากใน memmory ปัจจุบัน ถ้าเขียนเพิ่มอีกครั้ง มันก็ทำงานทั้งสองอัน
   };
+
+  //this.setState({ count: this.state.count + 1 }); คือการเซต state โดยนำไป merge กับ state เดิม  state = {count: 0,error: '',};
+
   // handleClichIncrement(){}
   handleClichDecrement = () => {
     this.setState({
@@ -63,7 +82,13 @@ class App extends Component {
     this.setState({ count: 0 });
   };
 
+  componentDidUpdate() {
+    console.log('App: componentDidUpdate');
+    //  ทำงานเหมือน useEffect(() => {}); ทำงานตอน render ตั้งแต่ครั้งที่ 2 ขึ้นไป
+  }
+
   render() {
+    console.log('App: render');
     const {
       state: { count },
     } = this; // {state:, handleClichIncrement, handleClichDecrement, handleClichReset}
@@ -71,16 +96,26 @@ class App extends Component {
       <div style={{ padding: '4rem' }}>
         <h1>Counter App</h1>
         {/* <h2>{this.state.count}</h2> */}
-        <CountText count={count} />
+        {this.state.showCounter && <CountText count={count} />}
         <button onClick={this.handleClichIncrement}>+</button>
         <button onClick={this.handleClichDecrement}>-</button>
         <button onClick={this.handleClichReset}>Reset</button>
+        <button onClick={() => this.setState({ showCounter: !this.state.showCounter })}>
+          Toggle
+        </button>
       </div>
     );
   }
 }
-
+// instance object ของ class App มี property และ method ทั้งหมดของ class App
 const app = new App();
 app.render();
+//เมื่อ new App() ถูกเรียกใช้งาน constructor จะทำงานทันที constructor(props) {
+//   super(props);
+//   this.state = {
+//     count: 0,
+//     error: '',
+//   };
+// } และทำงานแค่ครั้งแรกครั้งเดียว
 
 export default App;
--- CodeCamp9/React/hook/src/components/CountText.js
+++ CodeCamp9/React/hook/src/components/CountText.js
@@ -1,7 +1,32 @@
 import { Component } from 'react';
 
 class CountText extends Component {
+  constructor(props) {
+    super(props);
+    console.log('CountText: Constrctor');
+  }
+
+  componentDidMount() {
+    // setState() ใช้ได้ไม่มีปัญหา
+    console.log('CountText: componentDidMount');
+    // sideEffect จะทำงานในนี้เท่านั้น
+    // network request eg. axios.get();
+    // ทำงานเหมือน useEffect(() => {},[]); ทำงานแค่ครั้งแรกที่ render ครั้งเดียว
+  }
+
+  componentDidUpdate() {
+    //if(){
+    // setState() เกิด infinit re-render
+    // }
+    console.log('CountText: componentDidUpdate');
+    //  ทำงานเหมือน useEffect(() => {}); ทำงานตอน render ตั้งแต่ครั้งที่ 2 ขึ้นไป
+  }
+  componentWillUnmount() {
+    console.log('CountText: componentWillUnmount');
+    // ทำงานเมื่อมีการลบ component ออกจาก DOM ทั้งหมด
+  }
   render() {
+    console.log('CountText: render');
     const {
       props: { count },
     } = this;
